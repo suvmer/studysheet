792ebf20713c90239e9650bf8ba220e9e6909d8a
suvmer <suvmer@mail.ru>
2023-04-24 23:43:22 +0300

    multiple subject fields in work

--- client/src/components/CreateTable.jsx
+++ client/src/components/CreateTable.jsx
@@ -1,19 +1,87 @@
-import React from "react";
+import React, { useState } from "react";
 import classes from "../styles/UI/CreateTable.module.css";
 import { InfoBlock } from "./InfoBlock";
 
 export const CreateTable = () => {
-  return <><div className="midbox">
-  <mark className="big">Добавить расписание</mark>
-</div>
-<div className="btn" to="/my/add">Добавить существующее</div>
-  <div className="midbox center">
-    <mark className="big">Или</mark>
-  </div>
-<div className="event create">
   
-  <label for="name">Как назвать расписание?</label>
+  /*
+  const table = [[{
+    start: Date.now() + 20*60*1000 + 10000,
+    duration: 35,
+    name: "Пара математического анализа",
+    cabinet: "405",
+    teacher: "Ухалов Алексей Юрьевич",
+    place: "2 корпус(ул. Кирова, 8/10)"
+  }], [], [], [], [], [], []];*/
+  //const table = [[], [], [], [], [], [], []];
+  const days = ["Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"];
+
+  const getField = () => ({
+    start: Date.now(),
+    duration: 95,
+    name: "",
+    cabinet: "",
+    teacher: "",
+    place: "7 корпус(Союзная 144)" });
+
+  const [table, setTable] = useState([
+    ...Array(7).fill([getField()])
+  ]);
+
+  const addSubj = (index) => {
+    setTable({...table, index: [...table[index], getField()]});
+  };
+  console.log(table)
+  return (<>
+        {table.map((elem, index) => {
+          return (
+            <div key={index} className="mup">
+              <mark className="big center">{days[index]}</mark>
+              {elem.map((subj, ind) => (<div className="subject"><div className="mid">#{ind+1}.</div>
+                <input placeholder="Название" defaultValue={subj.name}/>
+                <input placeholder="Кабинет" defaultValue={subj.cabinet}/>
+                <input placeholder="Время начала" defaultValue={subj.start}/>
+                <input placeholder="Место" defaultValue={subj.place}/>
+              </div>)
+              )}
+              <div onClick={() => addSubj(index)} className="btn mup">Добавить предмет</div>
+            </div>
+          )
+        })}
+        </>);
+
+  const toPaste = [];
+  days.forEach((elem) => {
+    
+    toPaste.push(
+      <div className="mup">
+        <mark className="big center">{elem}</mark>
+        <div className="btn">Добавить предмет</div>
+      </div>
+    );
+  });
+
+  
+  return <>
+<div className="midbox">
+  <div className="big">Добавить расписание</div>
+</div>
+<div className="midbox">
+  <input className="full" id="idTable" placeholder="ID расписания"/><div className="btn">Добавить</div>
+</div>
+<hr className="mup"/>
+<mark className="big center">Или</mark>
+<hr/>
+<mark className="big center">Создать новое</mark>
+  
+<div className="event create">
+  <label htmlFor="name">Как назвать расписание?</label>
   <input id="name" placeholder="Название"/>
-  <div className="btn" to="/my/add">Добавить</div>
-</div></>;
+  <label htmlFor="hours">Сколько часов?</label>
+  <input id="hours" placeholder="Часы"/>
+  {toPaste}
+</div>
+  <div className="btn" to="/my/add">Создать</div>
+
+</>;
 };
