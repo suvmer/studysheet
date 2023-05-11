e271be7dcbda0051fbcb6f3de33880a4af12926c
duhan <duhan@LAPTOP-1BP174QR>
2023-05-11 20:30:35 +0300

    info + viewPage

---
+++ client/src/pages/ViewPage.jsx
@@ -0,0 +1,40 @@
+import React, { useEffect } from 'react'
+import { dateToString } from '../utils/utils';
+import { useDispatch, useSelector } from 'react-redux';
+import { Event } from '../components/Event';
+import { InfoBlock } from '../components/InfoBlock';
+import { NavLink, useOutlet } from 'react-router-dom';
+import { getTable } from '../components/actions/tables';
+import { getUser } from '../components/actions/users';
+
+export const ViewPage = () => {
+    const dispatch = useDispatch();
+    const tables = useSelector(state => state.profile.user.ownTables);
+    const users = useSelector(state => state.profile.users);
+    const current = useSelector(state => state.profile.currentTable);
+    const schedules = useSelector(state => state.table.schedules);
+    useEffect(() => {
+        tables.forEach(id => dispatch(getTable(id)));
+    }, []);
+
+    const outlet = useOutlet();
+    
+    
+    const ownTables =
+    tables.length > 0 ? 
+    <>
+    <div className="midbox">
+        <mark className="big">Ваши расписания</mark>
+        <NavLink className="btn" to="/my/add">Добавить</NavLink>
+    </div>
+    {bars} </> :
+    <>
+        <div className="midbox">
+            <mark className="big">Расписаний нет</mark>
+            <NavLink className="btn" to="/my/add">Добавить</NavLink>
+        </div>
+    </>;
+    return <div className="wall">
+    {outlet || ownTables}
+    </div>;
+}
