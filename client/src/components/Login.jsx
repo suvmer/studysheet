56678b9a25bb85795c4d6b4055ce015cbaf26ca2
suvmer <suvmer@mail.ru>
2023-05-24 00:10:18 +0300

    portal added

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,4 +1,5 @@
-import React, { useReducer, useState } from "react";
+import React, { useEffect } from "react";
+import { createPortal } from "react-dom";
 import { DarkButton } from "./UI/Buttons";
 
 export const Login = () => {
@@ -10,11 +11,28 @@ export const Login = () => {
   const handleChange = (idChange, idFor, namer, value) => {
     
   }
+
+  const mount = document.getElementById("portal");
+  const el = document.createElement("div");
+
+  useEffect(() => {
+    mount.appendChild(el);
+    return () => mount.removeChild(el);
+  }, [el, mount]);
+
+  return createPortal(<div className="login">
+  <input onChange={changeEv} name="name" type="email" placeholder="Почта"/>
+  <input onChange={changeEv} name="name" type="password" placeholder="Пароль"/>
+  <DarkButton>Войти</DarkButton>
+  </div>, el);
+};
+
+
+
+const Portal = ({children}) => {
   
+
  
-  return (<div className="login">
-        <input onChange={changeEv} name="name" type="email" placeholder="Почта"/>
-        <input onChange={changeEv} name="name" type="password" placeholder="Пароль"/>
-        <DarkButton>Войти</DarkButton>
-        </div>);
+
+  
 };
