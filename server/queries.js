a2af83f48c2e07d3abe157a3ad28e21c085069b9
duhan <duhan@LAPTOP-1BP174QR>
2023-06-02 08:42:50 +0300

    auto create user table test

--- server/queries.js
+++ server/queries.js
@@ -21,5 +21,21 @@ const pool = new Pool({
   port: 5432,
 })
 
+//const query = await connection.query('INSERT INTO users (name, email, password, "isActivated", "activationLink", info, "currentTable", regtime) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING id', [name, email, hashPassword, false, activationLink, info, -1, Date.now()])
+
+pool.query(`
+CREATE TABLE IF NOT EXISTS userr (
+  id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL,
+  password TEXT NOT NULL,
+  isActivated BOOLEAN,
+  activationLink TEXT NOT NULL,
+  info TEXT,
+  currentTable INTEGER,
+  regtime INTEGER
+)
+`)
+pool.query(`INSERT INTO userr(name, password, "isActivated", "activationLink", info, "currentTable", regtime) VALUES ("Мугома", "Абоба", TRUE, "n.ru", "{\"fdfadf\": \"fdf\"}", 0, ${Date.now()})`)
+
 module.exports = pool;
 //module.exports = {Sequelize, sequelize};
