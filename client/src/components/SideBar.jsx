2b0aaab657c935674451d47dd342140d927e1715
suvmer <suvmer@mail.ru>
2023-06-12 22:00:24 +0300

    Disabled drag for links and buttons

--- client/src/components/SideBar.jsx
+++ client/src/components/SideBar.jsx
@@ -2,8 +2,8 @@ import { NavLink } from "react-router-dom";
 
 export const SideBar = () => {
     return <aside>
-      <NavLink to="/" className={({isActive}) => isActive ? "active": ""}>Главная</NavLink>
-      <NavLink to="/my" className={({isActive}) => isActive ? "active": ""}>Моё расписание</NavLink>
-      <NavLink to="/account" className={({isActive}) => isActive ? "active": ""}>Аккаунт</NavLink>
+      <NavLink draggable="false" to="/" className={({isActive}) => isActive ? "active": ""}>Главная</NavLink>
+      <NavLink draggable="false" to="/my" className={({isActive}) => isActive ? "active": ""}>Моё расписание</NavLink>
+      <NavLink draggable="false" to="/account" className={({isActive}) => isActive ? "active": ""}>Аккаунт</NavLink>
     </aside>;
   }
