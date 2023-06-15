d70d810069147c56f1c6ab74901bb4d6ed0b2aac
duhan <duhan@LAPTOP-1BP174QR>
2023-06-15 13:34:32 +0300

    getClosest() to utils

--- client/src/components/OnlineTable.jsx
+++ client/src/components/OnlineTable.jsx
@@ -1,49 +1,10 @@
 import React from 'react'
-import { days, getDif, getTitle, msToNumbers, msToWords, shortDays } from '../utils/utils';
+import { days, getClosest, getTitle, msToNumbers, msToWords, shortDays } from '../utils/utils';
 import { useSelector } from 'react-redux';
 import { InfoBlock } from '../components/InfoBlock';
 import dayjs from 'dayjs';
 
 export const OnlineTable = ({table}) => {
-    
-    const getClosestSubject = (list, today = (((new Date()).getDay()+6)%7)) => {
-        var cursubj = null;
-        var mode = -1; //0 - before, 1 - in process
-        var dif = today - (((new Date()).getDay()+6)%7);
-        if(dif < 0)
-            dif += 7;
-        dif *= 86400000; //1 day = 1000*60*60*24 = 86400000ms
-        list[today].forEach(el => {
-            if(el.start + dif <= Date.now() && el.end + dif >= Date.now()) {
-                if(cursubj == null || el.start < cursubj.start) {
-                    cursubj = el;
-                    mode = 1;
-                }
-            }
-            if(el.start > Date.now()) {
-                if(mode != 1) {
-                    if(cursubj == null || el.start < cursubj.start) {
-                        cursubj = el;
-                        mode = 0;
-                    }
-                }
-            }
-        })
-        if(cursubj != null)
-            return [{...cursubj, start: cursubj.start + dif, end: cursubj.end + dif}, mode, today];
-        return [cursubj, mode, today];
-    }
-    const getClosest = (list) => {
-        const [cursubj, mode, today] = getClosestSubject(table.tables);
-        if(cursubj == null) { //not found upcoming events in today
-            for(var i = 1; i <= 7; i++) { //so find them in other days
-                const [nextSubj, nextMode, nextday] = getClosestSubject(list, (today + i)%7);
-                if(nextSubj != null) 
-                    return [nextSubj, nextMode, nextday];
-            }
-        }
-        return [cursubj, mode, today];
-    }
     //console.log(table)
     const [cursubj, mode, day] = getClosest(table.tables);
     const today = ((new Date()).getDay()+6)%7;
--- client/src/utils/utils.js
+++ client/src/utils/utils.js
@@ -153,5 +153,44 @@ export const getDifSign = (ts1, ts2) => {
   return ((dayjs().hour(d1.hour()).minute(d1.minute()).second(d1.second())).valueOf() - (dayjs().hour(d2.hour()).minute(d2.minute()).second(d2.second())).valueOf());
 }
 
+export const getClosestSubject = (list, today = (((new Date()).getDay()+6)%7)) => {
+  var cursubj = null;
+  var mode = -1; //0 - before, 1 - in process
+  var dif = today - (((new Date()).getDay()+6)%7);
+  if(dif < 0)
+      dif += 7;
+  dif *= 86400000; //1 day = 1000*60*60*24 = 86400000ms
+  list[today].forEach(el => {
+      if(el.start + dif <= Date.now() && el.end + dif >= Date.now()) {
+          if(cursubj == null || el.start < cursubj.start) {
+              cursubj = el;
+              mode = 1;
+          }
+      }
+      if(el.start > Date.now()) {
+          if(mode != 1) {
+              if(cursubj == null || el.start < cursubj.start) {
+                  cursubj = el;
+                  mode = 0;
+              }
+          }
+      }
+  })
+  if(cursubj != null)
+      return [{...cursubj, start: cursubj.start + dif, end: cursubj.end + dif}, mode, today];
+  return [cursubj, mode, today];
+}
+export const getClosest = (list) => {
+  const [cursubj, mode, today] = getClosestSubject(list);
+  if(cursubj == null) { //not found upcoming events in today
+      for(var i = 1; i <= 7; i++) { //so find them in other days
+          const [nextSubj, nextMode, nextday] = getClosestSubject(list, (today + i)%7);
+          if(nextSubj != null) 
+              return [nextSubj, nextMode, nextday];
+      }
+  }
+  return [cursubj, mode, today];
+}
+
 export const days = ["Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"];
 export const shortDays = ["Пн", "Вт", "Ср", "Чт", "Пт", "Сб", "Вс"];
