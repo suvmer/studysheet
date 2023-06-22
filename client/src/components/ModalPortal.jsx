1f0e4570cfeb986476130e5ff8918c22029ffe9d
suvmer <suvmer@mail.ru>
2023-06-22 16:07:09 +0300

    Modal Portal added

---
+++ client/src/components/ModalPortal.jsx
@@ -0,0 +1,38 @@
+import React, { useEffect, useRef, useState } from "react";
+import { createPortal } from "react-dom";
+import { setInfoOpen, setModal } from "../store/uiReducer";
+import { useOnClickOutside } from "../utils/ownHooks";
+import { useDispatch, useSelector } from "react-redux";
+import { sendNewInfo } from "./actions/users";
+import { DarkButton } from "./UI/Buttons";
+
+export const ModalPortal = ({content}) => {
+  const mount = document.body;
+  const el = document.createElement("div");
+
+  useEffect(() => {
+    mount.appendChild(el);
+    return () => mount.removeChild(el);
+  }, [el, mount]);
+  
+  const ref = useRef(null);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (ref.current.parentNode.className == "modal modal_open" && !ref.current.contains(event.target)) {
+        console.log("clicked")
+        dispatch(setModal(null))
+      }
+    };
+    document.addEventListener('click', handleClickOutside, true);
+    return () => {
+      document.removeEventListener('click', handleClickOutside, true);
+    };
+  }, []);
+
+  return createPortal(<div className={content ? "modal modal_open" : "modal"}>
+        <div className="modal_in" ref={ref}>
+            {content ?? ""}
+        </div>
+  </div>, mount);
+};
