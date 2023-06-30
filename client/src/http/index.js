fb1d2338a7fe954aa3802d0bdf10e4f7a56452e6
suvmer <suvmer@mail.ru>
2023-06-30 19:11:09 +0300

    new api_url

--- client/src/http/index.js
+++ client/src/http/index.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { checkAuth } from "../components/actions/users";
 import { store } from "../store";
 
-export const API_URL = 'http://194.87.238.182:5000/api';
+export const API_URL = 'http://studysheet.ru/api';
 export const CURRENT_URL = 'http://studysheet.ru';
 
 const api = axios.create({
