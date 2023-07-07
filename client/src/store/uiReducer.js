e40cb444e5da4438ddfc0cdcca1f0883f0d6f345
suvmer <suvmer@mail.ru>
2023-07-07 17:30:33 +0300

    refactor

--- client/src/store/uiReducer.js
+++ client/src/store/uiReducer.js
@@ -2,15 +2,11 @@ const defaultState = {
     time: Date.now(),
     modal: null,
     loginOpen: false,
-    chpwOpen: false,
-    infoOpen: false,
     menuOpen: false
 };
 
 export const SETTIME = "SETTIME";
 export const SETLOGIN = "SETLOGIN";
-export const SETCHWP = "SETCHWP";
-export const SETINFOOPEN = "SETINFOOPEN";
 export const SETMENUOPEN = "SETMENUOPEN";
 export const SETMODAL = "SETMODAL";
   
@@ -20,10 +16,6 @@ export const uiReducer = (state = defaultState, action) => {
             return {...state, time: action.payload};
         case SETLOGIN:
             return {...state, loginOpen: action.payload};
-        case SETCHWP:
-            return {...state, chpwOpen: action.payload};
-        case SETINFOOPEN:
-            return {...state, infoOpen: action.payload};
         case SETMENUOPEN:
             return {...state, menuOpen: action.payload};
         case SETMODAL:
@@ -34,7 +26,5 @@ export const uiReducer = (state = defaultState, action) => {
 
 export const setTime = (time) => ({type: SETTIME, payload: time});
 export const setLogin = (open) => ({type: SETLOGIN, payload: open});
-export const setChpw = (open) => ({type: SETCHWP, payload: open});
-export const setInfoOpen = (open) => ({type: SETINFOOPEN, payload: open});
 export const setMenuOpen = (open) => ({type: SETMENUOPEN, payload: open});
 export const setModal = (content) => ({type: SETMODAL, payload: content});
