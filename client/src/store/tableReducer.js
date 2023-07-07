160befbd10b56a4419bf69e07dfd569ffe682040
suvmer <suvmer@mail.ru>
2023-07-07 17:19:35 +0300

    refactoring starts

--- client/src/store/tableReducer.js
+++ client/src/store/tableReducer.js
@@ -5,23 +5,12 @@ const defaultState = {
   defs: [
     [['09:00', '10:35'], ['10:45', '12:20'], ['13:20', '14:55'], ['15:05', '16:40']]
   ],
-  tables: [],/*[
-    {
-      start: Date.now() + 20*60*1000 + 10000,
-      end: Date.now() + 20*60*1000 + 10000,
-      duration: 35,
-      name: "Пара математического анализа",
-      cabinet: "405",
-      teacher: "Ухалов Алексей Юрьевич",
-      place: "2 корпус(ул. Кирова, 8/10)"
-    },
-  ],*/
+  tables: [],
   schedules: [],
   users: []
 };
 
 export const tableReducer = (state = defaultState, action) => {
-  //if (action.type != "INITIALIZE" && !state.isInitialized) return state;
   switch (action.type) {
     case "INITIALIZE":
       return action.payload;
