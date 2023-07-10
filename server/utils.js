a1bf1dbd4aae6bb7ba2129494d085dce10313395
suvmer <suvmer@mail.ru>
2023-07-10 22:43:55 +0300

    deprecated

--- server/utils.js
+++ server/utils.js
@@ -14,18 +14,6 @@ class utils {
     days = ["Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"];
     shortDays = ["Пн", "Вт", "Ср", "Чт", "Пт", "Сб", "Вс"];
 
-    prepareSqlKeys(values) { //doing $1 $2 ... for values (for encapsulating)
-        let res = []
-        let sep = Object.values(values[0]).length;
-        let nums = [...Array(values.length*sep).keys()].map(x => x+1);
-        for(var i = 0; i < values.length; i++)
-            res.push(`($${nums.slice(i*sep, (i+1)*sep).join(', $')})`);
-        return res;
-    }
-    prepareSqlDate(date) {
-        return new Date(date).toISOString().slice(0, 19).replace('T', ' ');
-    }
-
     withACapital(str) {
         return str.toLowerCase().replace(/(^[a-zа-яё]{1})|(\s+[a-zа-яё]{1})/g, letter => letter.toUpperCase())
     }
